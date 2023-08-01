689b492152f0bc67912df0fea0cb0a163256d1e4
EnU098 <ergashevnodir1998@gmail.com>
2023-08-01 09:18:17 +0500

    fff

--- newProject.sql
+++ newProject.sql
@@ -1,34 +1,31 @@
 /*--------------------N.N-------------------*/
-create table position(
-    id serial primary key ,
+create table position
+(
+    id   serial primary key,
     name varchar
 );
-create table company(
-    id serial primary key ,
-    name varchar,
+create table company
+(
+    id       serial primary key,
+    name     varchar,
     location varchar
 );
-create table company_position(
-    company_id int references company(id),
-    position_id int references position(id)
+create table company_position
+(
+    company_id  int references company (id),
+    position_id int references position (id)
 );
 
-create table job(
-    id serial primary key ,
-    job_type varchar,
-    company_id int references company(id),
-    salary double precision,
-    title varchar,
-    location varchar,
+create table job
+(
+    id          serial primary key,
+    job_type    varchar,
+    company_id  int references company (id),
+    salary      double precision,
+    title       varchar,
+    location    varchar,
     data_posted date
 );
-
-<<<<<<<<< Temporary merge branch 1
-
-
-create table comment(
-    id serial primary key
-=========
 create table users(
   id serial primary key ,
   email varchar,
@@ -39,40 +36,43 @@ create table users(
   date_od_brith date,
   country varchar,
   gender gender
->>>>>>>>> Temporary merge branch 2
-);
+  );
 /*--------------------N.N-------------------*/
 
 
 /*--------------E.Nodir--------------*/
 
-create table post(
-                     id serial primary key,
-                     user_id int references users(id),
-                     context text,
-                     photo varchar,
-                     date date
+create table post
+(
+    id      serial primary key,
+    user_id int references users (id),
+    context text,
+    photo   varchar,
+    date    date
 );
 
-create table post_like(
-                          id serial primary key ,
-                          user_id int references users(id),
-                          post_id int references post(id)
+create table post_like
+(
+    id      serial primary key,
+    user_id int references users (id),
+    post_id int references post (id)
 );
 
-create table comment(
-                        id serial primary key ,
-                        user_id int references users(id),
-                        text varchar,
-                        post_id int references post(id),
-                        date_posted date,
-                        reply_id int references users(id)
+create table comment
+(
+    id          serial primary key,
+    user_id     int references users (id),
+    text        varchar,
+    post_id     int references post (id),
+    date_posted date,
+    reply_id    int references users (id)
 );
 
-create table comment_like(
-                             id serial primary key ,
-                             user_id int references users(id),
-                             comment_id int references comment(id)
+create table comment_like
+(
+    id         serial primary key,
+    user_id    int references users (id),
+    comment_id int references comment (id)
 );
 
 /*--------------E.Nodir--------------*/
