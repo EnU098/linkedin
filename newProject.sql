a1f8f68724d152ea26f4699c79773e91dbfd5401
Nodirbek <no0404ir@gmail.com>
2023-08-01 14:52:49 +0500

    add function

--- newProject.sql
+++ newProject.sql
@@ -2,42 +2,43 @@
 create table position
 (
     id   serial primary key,
-    name varchar
+    name varchar not null
 );
 create table company
 (
     id       serial primary key,
-    name     varchar,
-    location varchar
+    name     varchar not null,
+    location varchar not null
 );
 create table company_position
 (
-    company_id  int references company (id),
-    position_id int references position (id)
+    company_id  int references company (id) not null,
+    position_id int references position (id) not null
 );
 
 create table job
 (
     id          serial primary key,
-    job_type    varchar,
-    company_id  int references company (id),
+    job_type    varchar not null,
+    company_id  int references company (id) not null,
     salary      double precision,
-    title       varchar,
-    location    varchar,
-    data_posted date
+    title       varchar not null,
+    location    varchar not null,
+    data_posted date not null
 );
 
 create type gender as ENUM('f','m');
 create table users(
   id serial primary key ,
-  email varchar,
-  password varchar,
-  first_name varchar,
-  last_name varchar,
-  photo_link varchar,
-  date_od_brith date,
-  country varchar,
-  gender gender
+  email varchar unique not null,
+  password varchar not null,
+  first_name varchar not null,
+  last_name varchar not null,
+  photo_link varchar not null,
+  date_od_brith date not null,
+  country varchar not null,
+  phone_number varchar unique not null ,
+  gender gender not null
   );
 /*--------------------N.N-------------------*/
 
@@ -47,34 +48,34 @@ create table users(
 create table post
 (
     id      serial primary key,
-    user_id int references users (id),
-    context text,
-    photo   varchar,
-    date    date
+    user_id int references users (id) not null,
+    context text not null,
+    photo   varchar not null,
+    date    date not null
 );
 
 create table post_like
 (
     id      serial primary key,
-    user_id int references users (id),
-    post_id int references post (id)
+    user_id int references users (id) not null,
+    post_id int references post (id) not null
 );
 
 create table comment
 (
     id          serial primary key,
-    user_id     int references users (id),
-    text        varchar,
-    post_id     int references post (id),
-    date_posted date,
-    reply_id    int references users (id)
+    user_id     int references users (id) not null,
+    text        varchar not null,
+    post_id     int references post (id)not null,
+    date_posted date not null,
+    reply_id    int references users (id) not null
 );
 
 create table comment_like
 (
     id         serial primary key,
-    user_id    int references users (id),
-    comment_id int references comment (id)
+    user_id    int references users (id) not null,
+    comment_id int references comment (id) not null
 );
 
 /*--------------E.Nodir--------------*/
@@ -83,14 +84,14 @@ create table comment_like
 
 create table connections(
     id serial primary key ,
-    user1 int references users(id),
-    user2 int references users(id)
+    user1 int references users(id)not null,
+    user2 int references users(id) not null
 );
 
 create table endorses(
     id serial primary key ,
-    user1 int references users(id),
-    user2 int references users(id),
+    user1 int references users(id) not null,
+    user2 int references users(id)not null,
     date_posted date
 );
 ------------------Davron-------------------
@@ -98,33 +99,34 @@ create table endorses(
 /*---------------N.N------------------------*/
 create table message(
   id serial primary key,
-  sender_user_id int references users(id),
-  resiver_user_id int references users(id),
-  message_body varchar
+  sender_user_id int references users(id) not null,
+  resiver_user_id int references users(id) not null,
+  message_body varchar not null
 );
 create table user_message(
-  user_id int references users(id),
-  message_id int references message(id)
+  user_id int references users(id) not null ,
+  message_id int references message(id) not null
 );
 create table experience(
   id serial primary key ,
-  user_id int references users(id),
-  start_date date ,
-  end_date date,
-  company_id int references company(id),
-  position_id int references position(id)
+  user_id int references users(id) not null ,
+  start_date date not null ,
+  end_date date not null ,
+  company_id int references company(id) not null ,
+  position_id int references position(id) not null
 );
 create table skill(
-    id serial primary key ,
-    name varchar
+    id serial primary key not null,
+    name varchar not null
 );
 create table user_skills(
-    user_id int references users(id),
-    skill_id int references skill(id)
+    user_id int references users(id) not null ,
+    skill_id int references skill(id) not null
 );
 
 create table endorses_skill(
-    endorses_id int references endorses(id),
-    skill_id int references skill(id)
+    endorses_id int references endorses(id) not null ,
+    skill_id int references skill(id) not null
 );
+
 /*---------------N.N------------------------*/
